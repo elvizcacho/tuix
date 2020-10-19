baedc303d0d0d1f629cd03a55473f96be97dc448
Juan Vizcaino <elvizcacho@gmail.com>
2020-10-19 20:29:10 +0200

    class 22

--- class19/Homework/script.js
+++ class19/Homework/script.js
@@ -3,47 +3,47 @@ let mSec = 0
 let sec = 0
 let min = 0
 
-let isStopwatchOn = false
+let isStopwatchOn = true
 console.log(isStopwatchOn)
 const stopwatch = setInterval(function() {
     if (isStopwatchOn === true) {
-        
+
         mSec ++
         //console.log(mSec)
-    
+
         if (mSec === 100) {
             mSec = 0
             sec ++
-    
+
             //console.log(sec)
         }
         if (sec === 60) {
             sec = 0
             min ++
-    
+
             //console.log(min)
         }
-    
+
         if (mSec < 10) {
             displayMSec = '0' + mSec
         } else {
             displayMSec = mSec
         }
-    
+
         if (sec < 10) {
             displaySec = '0' + sec
         } else {
             displaySec = sec
         }
-    
+
         if (min < 10) {
             displayMin = '0' + min
         } else {
             displayMin = min
         }
-    
+
         console.log(displayMin + ':' + displaySec + ':' + displayMSec)
-        
+
     }
 }, 10)
 
---
+++ class22/README.md
@@ -0,0 +1,35 @@
+# Class 14
+
+#### Topics: 
+- Review of previous concepts
+- Math.random()
+
+### Homework
+- Highlight 
+_ Create an analog stopwatch with css and HTML.
+
+TASK:
+
+n = 1
+ *
+* *
+ *
+n = 2
+  **
+ *  *
+ *  *
+  **
+n = 3
+   ***   
+  *   *
+  *   *
+  *   *
+   ***
+n = 4
+   ****   
+  *    *
+  *    *
+  *    *
+  *    * 
+   ****
+
